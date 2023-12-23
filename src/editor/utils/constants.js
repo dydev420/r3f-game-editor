5fd791b7ae43c0b5a645823339ada40d72b3c762
DY Dev <dydev420@gmail.com>
2023-12-24 02:21:19 +0530

    ignore previous commit message it had other changes too

--- src/editor/utils/constants.js
+++ src/editor/utils/constants.js
@@ -9,3 +9,28 @@ export const TRANSFORM_MODES = ['translate', 'rotate', 'scale'];
  */
 export const GRID_SIZE = 8;
 export const BLOCK_SIZE = 1;
+export const BLOCK_FLOAT_HEIGHT = 0.3;
+
+/**
+ * Block Terrain Config
+ */
+export const TERRAIN_TYPES = [
+  'grass',
+  'ground',
+  'water',
+  'road'
+]
+export const TERRAIN_MATERIALS = {
+  grass: {
+    color: 'LimeGreen'
+  },
+  ground: {
+    color: 'SandyBrown'
+  },
+  water: {
+    color: 'CornflowerBlue'
+  },
+  road: {
+    color: 'Black'
+  },
+}; 
